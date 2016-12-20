e71bde84f6a1f2d13bebb40990467a2c4c669e0a
Luke Storry <luke@lukestorry.co.uk>
2016-12-20 00:29:42 +0000

    started convex hull challenge

---
+++ Functional/Recursion/ConvexHull.hs
@@ -0,0 +1,6 @@
+-- https://www.hackerrank.com/challenges/convex-hull-fp
+-- Input:
+--  First line of input will contain a integer, N, number of points.
+--  Then follow N lines where each line contains the coordinate, xi yi, of ith point.
+-- Output: the perimeter of the convex hull 
+
