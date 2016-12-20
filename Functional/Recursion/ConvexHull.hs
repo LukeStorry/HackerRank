b8aeb4b12e15483a4cf801a03542eca504953b71
Luke Storry <luke@lukestorry.co.uk>
2016-12-20 00:42:48 +0000

    added io

--- Functional/Recursion/ConvexHull.hs
+++ Functional/Recursion/ConvexHull.hs
@@ -4,5 +4,13 @@
 --  Then follow N lines where each line contains the coordinate, xi yi, of ith point.
 -- Output: the perimeter of the convex hull 
 
+import Text.Printf
+
 distance :: (Int, Int) -> (Int, Int) -> Double
 distance (x1, y1) (x2, y2) = sqrt $ fromIntegral $ (x1-x2)^2 + (y1-y2)^2
+
+main :: IO ()
+main = do
+  n <- readLn :: IO Int
+  points <- getContents
+  printf "%.1f\n" $ solve $ map (\[x, y] -> (x, y)) . map (map (read::String->Int)) . map words . lines $ points
