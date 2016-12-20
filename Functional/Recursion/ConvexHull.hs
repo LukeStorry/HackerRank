cb7f931c12b3aa38690939dba556060f90336c40
Luke Storry <luke@lukestorry.co.uk>
2016-12-20 00:55:22 +0000

    started gift wrapping algorithm

--- Functional/Recursion/ConvexHull.hs
+++ Functional/Recursion/ConvexHull.hs
@@ -6,9 +6,29 @@
 
 import Text.Printf
 
+-- distance between two points
 distance :: (Int, Int) -> (Int, Int) -> Double
 distance (x1, y1) (x2, y2) = sqrt $ fromIntegral $ (x1-x2)^2 + (y1-y2)^2
 
+-- folds list of points using distance
+totaldistance :: [(Int, Int)] -> Double
+totaldistance [] = 0
+totaldistance (a:b:s) = totaldistance (b:s) + distance a b
+
+-- angle between three points
+angle :: (Int, Int) -> (Int, Int) -> (Int, Int) -> Double
+angle _ _ _ = 0
+
+
+--add point with max angle to list, then move there and iterate.
+hull :: [(Int, Int)] -> [(Int, Int)]
+hull points = points
+
+--overall solving function
+solve :: [(Int, Int)] -> Double
+solve points = totaldistance . hull $ points
+
+-- io
 main :: IO ()
 main = do
   n <- readLn :: IO Int
