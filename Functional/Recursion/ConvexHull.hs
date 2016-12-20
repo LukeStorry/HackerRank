d7bff14e0760f66751732039f293ce92312fb24d
Luke Storry <luke@lukestorry.co.uk>
2016-12-20 00:29:55 +0000

    added distance calculator

--- Functional/Recursion/ConvexHull.hs
+++ Functional/Recursion/ConvexHull.hs
@@ -4,3 +4,5 @@
 --  Then follow N lines where each line contains the coordinate, xi yi, of ith point.
 -- Output: the perimeter of the convex hull 
 
+distance :: (Int, Int) -> (Int, Int) -> Double
+distance (x1, y1) (x2, y2) = sqrt $ fromIntegral $ (x1-x2)^2 + (y1-y2)^2
