b72b2dec7c0b6c7183200daead5397a6f4c916e8
Luke Storry <luke@lukestorry.co.uk>
2017-01-05 19:02:47 +0000

    Create Strings.cpp

---
+++ Cpp/Strings/Strings.cpp
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/c-tutorial-strings
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    
+    cout << a.length() << ' ' << b.length() << endl;
+    cout << a + b << endl;
+    swap(a[0], b[0]);
+    cout << a << ' ' <<  b << endl;
+    
+    return 0;
+}
